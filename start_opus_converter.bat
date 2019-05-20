1cd5d5a92bfde6c0eec5aeaa7a92a284df884727
Vlad Blazhko <mr_plum@mail.ru>
2019-05-20 10:50:14 +0200

    Make runner more suitable for end-users

--- start_opus_converter.bat
+++
@@ -1,6 +0,0 @@
-SET python_path="C:/miniconda3/python"
-SET converter_path="%userprofile%/Desktop/opus-converter/opus_converter.py"
-
-%python_path% %converter_path% . --format csv --split --one-file --save-inplace
-
-pause
